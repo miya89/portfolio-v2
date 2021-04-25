460714c4dc03e8d9e48a0b1e34a2e1a252946d80
MiyaPeynova <maria.peynova@hotmail.com>
2021-04-25 10:28:34 +0100

    Change animation and toggle on contact page

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@
 let checkbox = document.getElementById('checkbox');
 
 
+
 checkbox.addEventListener('change', () => {
     document.querySelector(".main").classList.toggle('contrast');
     document.querySelector(".btn").classList.toggle('secondary');
-    document.querySelector(".contact-main").classList.toggle('contrast');
+    
 });
